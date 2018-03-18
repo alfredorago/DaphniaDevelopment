451744ee40c280f2ac5d4fbe3f0398d7b14c66c4
ARago <alfredo.rago@gmail.com>
2018-03-18 16:41:39 +0100

    Now creates directory for results before running Kallisto quant

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -6,9 +6,9 @@ module load OpenMPI/1.10.2
 module load kallisto
 
 # Create dated directory
-ResultDir="../Results/$(date +"%Y%m%d")/Kallisto"
+ResultDir="../Results/$(date +"%Y%m%d")/Kallisto/F58"
 mkdir -p $ResultDir
 echo "Results directory set to $ResultDir"
 
 # Quantify transcripts (testing one folder only)
-kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir/Kallisto/F58 --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
+kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
