d2ff6bce6f454366c6f924ef4296f0077ee8c9ce
ARago <alfredo.rago@gmail.com>
2018-03-18 19:13:07 +0100

    Added second RNAseq run

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -20,6 +20,6 @@ while read sample; do
   echo -e "\nProcessing ${sample}"
   SampleDir=$ResultDir/${sample}
   mkdir -p $SampleDir
-  kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz
+  kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz ../SourceDatasets/Alfredo_full_data/161102_D00200_0297_BH573CBCXY/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz
 #  kallisto quant -t 4 -b 100 -i Mus_musculus_GRCm38.idx -o results/"${sample}"-aligned --single -l 180 -s 30 "${sample}"
 done <../SourceDatasets/Sample_IDs/SampleIDs.txt
