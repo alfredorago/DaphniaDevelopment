6a7eee67f381df1e77bd37e3dac54b069170b1e9
ARago <alfredo.rago@gmail.com>
2018-03-18 16:57:47 +0100

    Making sample location different from reference location

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -6,9 +6,10 @@ module load OpenMPI/1.10.2
 module load kallisto
 
 # Create dated directory
-ResultDir="../Results/$(date +"%Y%m%d")/Kallisto/F58"
+ResultDir="../Results/$(date +"%Y%m%d")/Kallisto"
 mkdir -p $ResultDir
+mkdir -p $ResultDir/F58
 echo "Results directory set to $ResultDir"
 
 # Quantify transcripts (testing one folder only)
-kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
+kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir/F58 --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/SampleF58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
