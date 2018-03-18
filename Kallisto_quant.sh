f53fbef37d35806883aac9d8273fa670759fea2b
ARago <alfredo.rago@gmail.com>
2018-03-18 18:31:42 +0100

    Process all samples from first sequencing run using loop

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -20,8 +20,6 @@ while read sample; do
   echo -e "\nProcessing ${sample}"
   SampleDir=$ResultDir/${sample}
   mkdir -p $SampleDir
+  kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz
 #  kallisto quant -t 4 -b 100 -i Mus_musculus_GRCm38.idx -o results/"${sample}"-aligned --single -l 180 -s 30 "${sample}"
 done <../SourceDatasets/Sample_IDs/SampleIDs.txt
-
-# Quantify transcripts (testing one folder only)
-#kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir/F58 --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_F58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_F58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
