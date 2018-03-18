a4e8be8f6131d0d73dced0b9fe540b3cc5929128
ARago <alfredo.rago@gmail.com>
2018-03-18 19:24:18 +0100

    Added explicit encoding of bootstraps and threads at beginning of script, removed text only option

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -5,6 +5,10 @@ module load GCC/4.9.3-2.25
 module load OpenMPI/1.10.2
 module load kallisto
 
+# Store run parmeters
+$Bootstraps = 500
+$Threads = 4
+
 # Create dated directory
 ResultDir="../Results/$(date +"%Y%m%d")/Kallisto"
 mkdir -p $ResultDir
@@ -21,5 +25,4 @@ while read sample; do
   SampleDir=$ResultDir/${sample}
   mkdir -p $SampleDir
   kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz ../SourceDatasets/Alfredo_full_data/161102_D00200_0297_BH573CBCXY/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz
-#  kallisto quant -t 4 -b 100 -i Mus_musculus_GRCm38.idx -o results/"${sample}"-aligned --single -l 180 -s 30 "${sample}"
 done <../SourceDatasets/Sample_IDs/SampleIDs.txt
