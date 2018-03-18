c8a426424e56d98bd256aeacad7f71f969b822a8
ARago <alfredo.rago@gmail.com>
2018-03-18 18:10:38 +0100

    Testing using wildcard for all .fastq.gz files in directory as kallisto quant input

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -12,4 +12,4 @@ mkdir -p $ResultDir/F58
 echo "Results directory set to $ResultDir"
 
 # Quantify transcripts (testing one folder only)
-kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir/F58 --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_F58/F58_CTGAAGC-ATAGAGG_L002_R1_001.fastq.gz ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_F58/F58_CTGAAGC-ATAGAGG_L002_R2_001.fastq.gz
+kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$ResultDir/F58 --threads=4 --plaintext ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_F58/*.fastq.gz
