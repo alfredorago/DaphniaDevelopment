83b99634f40541ae14aad874b3b94d5417f2ac94
ARago <alfredo.rago@gmail.com>
2018-03-19 08:05:55 +0100

    Find all BGI files with sample and pass on to Kallisto quant

--- Kallisto_quant.sh
+++ Kallisto_quant.sh
@@ -23,5 +23,7 @@ while read sample; do
   echo -e "\nProcessing ${sample}"
   SampleDir=$ResultDir/${sample}
   mkdir -p $SampleDir
-  kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=$Threads -b=Bootstraps ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz ../SourceDatasets/Alfredo_full_data/161102_D00200_0297_BH573CBCXY/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz  ../SourceDatasets/Alfredo_full_data/F17FTSEUET0053-03-20170720/Raw/${sample}/*.fq.gz  ../SourceDatasets/Alfredo_full_data/F17FTSEUET0053-07_DAPoxiR/Raw/${sample}/*.fq.gz  ../SourceDatasets/Alfredo_full_data/F17FTSEUET0053-14_DAPvswE/Raw/${sample}/*.fq.gz
+# Find all BGI files which match input sample and pass to Kallisto quant
+  BGI_runs=$(find ../SourceDatasets/Alfredo_full_data/*/*/${sample}.fq.gz)
+  kallisto quant --index=$ResultDir/Dmagna_OrsiniSIRV_index.idx --output-dir=$SampleDir --threads=$Threads -b=Bootstraps ../SourceDatasets/Alfredo_full_data/160922_D00255_0272_BHWLLTBCXX/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz ../SourceDatasets/Alfredo_full_data/161102_D00200_0297_BH573CBCXY/Project_KT_Alfredo/Sample_${sample}/*.fastq.gz $BGI_runs
 done <../SourceDatasets/Sample_IDs/SampleIDs.txt
