38bedd587d1822740f918eb985e12f15cf7321b5
ARago <alfredo.rago@gmail.com>
2018-03-21 20:15:28 +0100

    Corrected function to variable assignment

--- Read_compiler_join.sh
+++ Read_compiler_join.sh
@@ -6,14 +6,14 @@ mkdir -p $ResultDir
 echo "Results directory set to $ResultDir"
 
 # Store names of files containing sample data
-samples=ls ../Results/20180319/Kallisto/tsv/*.tsv
+samples=(ls ../Results/20180319/Kallisto/tsv/*.tsv)
 
 # Calculate number of sample datasets
-nSamples=ls -1 ../Results/20180319/Kallisto/tsv/*.tsv | wc -l
+nSamples=(ls -1 ../Results/20180319/Kallisto/tsv/*.tsv | wc -l)
 # Choose which column contains the data
-dataCol= 4
+dataCol=4
 # Generate indexes of columns of interest across all datasets to be joined
-dataCols= seq 1.$dataCol 1 $nSamples.$dataCol
+dataCols=(seq 1.$dataCol 1 $nSamples.$dataCol)
 
 # Join all datasets and save as tsv
 join ${samples} -o 1.1 $dataCols > $ResultDir/EST_counts.tsv
